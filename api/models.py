118706a2a55fc17967bb5938397903325b0beb19
manasa2604 <99647151+manasa2604@users.noreply.github.com>
2023-01-09 00:09:01 +0530

    Add files via upload

---
+++ api/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+class Note(models.Model):
+    body = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
+# Create your models here.
