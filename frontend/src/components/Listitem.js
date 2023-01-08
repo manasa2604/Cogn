7504f16939ea17d58d3957f271d4bd30c2628c5e
manasa2604 <99647151+manasa2604@users.noreply.github.com>
2023-01-09 00:52:57 +0530

    Add files via upload

---
+++ frontend/src/components/Listitem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+let getTime = (note) => {
+    return new Date(note.updated).toLocaleDateString()
+}
+
+let getTitle = (note) => {
+
+    let title = note.body.split('\n')[0]
+    if (title.length > 45) {
+        return title.slice(0, 45)
+    }
+    return title
+}
